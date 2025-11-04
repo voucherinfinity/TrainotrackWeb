4ba3155a3a339e1d1c99e410b7f1ddcc10e2488a
saurabhtrainotrack <saurabh.trainotrack@gmail.com>
2025-11-04 15:01:02 +0530

    Add project files.

---
+++ TrainotrackWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TrainotrackWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
